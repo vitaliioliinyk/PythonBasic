5e643680da8d8e7388dae9f8881ca25169255386
Vitalii Oliinyk <voliinyk@ford.com>
2023-01-30 00:50:25 +0200

    numbers

--- lesson_02/numbers.py
+++ lesson_02/numbers.py
@@ -3,6 +3,7 @@ import math
 # Метод print() для виведення на екран.
 print("Hello World!")
 print("The", "quick", "brown", "fox")  # кілька аргументів
+print("The", 1, True)  # кілька типів даних
 
 # Метод input() для отримання вводу від користувача
 # age = input("Введіть ваш вік: ")
@@ -12,12 +13,14 @@ print(f"Ваш вік {age} роки.") # в фігурних дужках ми 
 
 
 # Тип даних змінної
-print("Type of variable", type(age)) # <class 'str'>
+print("Type of variable", age, type(age))  # <class 'str'>
 age = int(age)
-print("Type of variable", type(age)) # <class 'int'>
+print("Type of variable", age, type(age))  # <class 'int'>
+age = float(age)
+print("Type of variable", age, type(age))  # <class 'int'>
 
 
-# Типи даних в Python
+# Числа в Python
 #   Цілі числа
 i = 1 # i type <class 'int'>
 print("i type", type(i))
@@ -35,6 +38,33 @@ b = 0.2
 print(f"Float {a} + {b} = {a+b}") # обчислення на f-string 0.30000000000000004
 print("Equal float", math.isclose(a+b, 0.3)) # перевірити чи співпадають
 #   Або використовувати тип Decimal. Інші типи Fraction i Complex
+
+
+# Арифметичні оператори
+#   + додавання  - бінарний та унарний
+#   - віднімання - бінарний та унарний
+#   * множення
+#   / ділення 10 / 2 = 5.0
+#   ** степінь
+#   // цілочисельне ділення 10//3=3; -10//3=-4; 15.5//3=5.0
+#   % залишок від ділення 10%3=1
+
+# Розширені оператори присвоювання
+#   += додавання
+#   -= віднімання
+#   *= множення
+#   /= ділення
+#   **= степінь
+#   //= цілочисельне ділення
+#   %= залишок від ділення
+c = 1
+c += 2
+print("Value of c: ", c)
+
+
+
+
+
 
 
 
