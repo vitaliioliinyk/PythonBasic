95671ac3d4498f8fe83a06f8ac706ea10d5b28da
Vitalii Oliinyk <voliinyk@ford.com>
2023-01-30 02:29:15 +0200

    strings

---
+++ lesson_02/comparison_operators.py
@@ -0,0 +1,12 @@
+b = True
+c = False
+d = 1 > 2
+print("Is 1 > 2 - ", d)
+
+# >  - 1 > 2 True
+# <  - 1 < 2 False
+# >= - 1 >= 2 True
+# <= - 1 <= 1 True
+# == - 1 == 1 True
+# != - 2 != 1 True
+d = 1 > 2
